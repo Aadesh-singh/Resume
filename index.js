3ded1f1e4aa7d222d0085feb36378aad81c534d8
Aadesh-singh <aadeshsingh146@gmail.com>
2021-04-29 15:16:42 +0530

    animated moving skills section is added

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ for(var i=0 ; i< navMenuAnchorTagId.length; i++){    //this line is only adding 
         },15)
     });
 }
-
-
-
-
 /*
 in case when we intent to pass function in html then we can use this code it will work exactly same as above..
 
@@ -69,3 +65,64 @@ html code will be
 //         window.scrollBy(0,30);
 //     },15)
 // }
+
+
+
+
+// fucntion through which skills section will shrink or grow..
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+
+
+// This function uses a for loop for individual progress bars.
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
